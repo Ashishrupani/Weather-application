5a4eb22297856122dfd301d53d8021594b8ba30f
Ashishrupani <143018180+Ashishrupani@users.noreply.github.com>
2025-09-16 08:07:52 -0500

    fixed scheduled job.

--- controller/cron.js
+++ controller/cron.js
@@ -1,13 +1,10 @@
 import cron from 'cron';
-import https from 'https';
 
 
-const job = new cron.CronJob("*/14 * * * *",function(){
-    https.get(process.env.CRON_URL, (res) => {
-        if (res.statusCode === '200') console.log("Request sent..");
-        else console.log("Error sending request..");
-    })
-    .on("error", (e)=> console.error("Error while sending request",e));
+const job = new cron.CronJob("*/14 * * * *", async function(){
+    console.log(`Running scheduled job at ${new Date()}}`);
+    const response = await fetch(`${process.env.CRON_URL}`);
+    console.log(response.status); 
 });
 
 export default job;
